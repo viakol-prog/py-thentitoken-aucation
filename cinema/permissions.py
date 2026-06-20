8174781b45df4f5a59e7310a5d5235da004de562
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-06-20 15:33:54 +0300

    thentitoken-aucation

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -4,5 +4,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            return True
+            return request.user and request.user.is_authenticated
         return request.user and request.user.is_staff
